e905ae3ca0c7481899b69fcc78e23a73ed511885
harshkumar1302 <bs824536@gmail.com>
2023-07-01 16:28:35 +0530

    Arrow Function

--- Fundamental - part 2/starter/script.js
+++ Fundamental - part 2/starter/script.js
@@ -26,31 +26,51 @@
 
 //Function declaration
 
-let age;
-const currentYear = 2023;
-const harshAge = calcAge1(1999);
+// let age;
+// const currentYear = 2023;
+// const harshAge = calcAge1(1999);
 
-function calcAge1(birthYear) {
-    age = currentYear - birthYear;
-    return age
-}
-console.log(harshAge);
+// function calcAge1(birthYear) {
+//     age = currentYear - birthYear;
+//     return age
+// }
+// console.log(harshAge);
 
 
-function calcAge2(birthYear) {
-    return currentYear - birthYear;
-}
-const age1 = calcAge2(1991);
-console.log(age1);
+// function calcAge2(birthYear) {
+//     return currentYear - birthYear;
+// }
+// const age1 = calcAge2(1991);
+// console.log(age1);
 
 
 // Function Expression
 
-const calcAge3 = function (birthYear) {
-    return currentYear - birthYear;
+// const calcAge3 = function (birthYear) {
+//     return currentYear - birthYear;
+// }
+// const age2 = calcAge3(1991);
+// console.log(age2);
+
+
+// Arrow Function
+const currentYear = 2023;
+
+const calcAge4 = birthYear => currentYear - birthYear;
+const age3 = calcAge4(1999);
+console.log(age3);
+
+
+
+const yearLeftForRetirement = (birthYear, firstName) => {
+    const age = currentYear - birthYear;
+    const retirement = 65 - age;
+    return `${firstName} retires in ${retirement} years.`;
+    // return retirement;
 }
-const age2 = calcAge3(1991);
-console.log(age2);
-
-
-
+const workyearLeft1 = yearLeftForRetirement(1999, 'Harsh');
+const workyearLeft2 = yearLeftForRetirement(2001, 'Sachindra');
+const workyearLeft3 = yearLeftForRetirement(1997, 'Dhirendar');
+console.log(workyearLeft1);
+console.log(workyearLeft2);
+console.log(workyearLeft3);
